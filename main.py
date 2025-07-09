6332c3b68ce208d49a6e1351ead7a652a8bba4fb
grace3721 <gracek3721@gmail.com>
2025-07-09 10:55:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,16 @@ st.header("Streamlit에 오신 것을 환영합니다!")
 st.write("이것은 간단한 텍스트입니다.")
 
 st.success("성공적으로 앱이 실행되었습니다! 🎉")
+import streamlit as st
+
+import numpy as np
+
+from astropy.io import fits
+
+from PIL import Image
+
+from astropy.coordinates import SkyCoord, EarthLocation, AltAz
+
+from astropy.time import Time
+
+from datetime import datetime
